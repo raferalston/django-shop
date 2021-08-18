326d3192a08780004dc846f62daafb4aa449f9ee
raferalston <like2pwnyou@yandex.ru>
2021-08-18 11:31:20 +0300

    upd

--- shop_application/shop/signals.py
+++ shop_application/shop/signals.py
@@ -4,12 +4,12 @@ from django.dispatch import receiver
 from django.db.models.signals import post_save, post_delete
 
 @receiver(post_save, sender=ItemArticle)
-def news_save_handler(sender, **kwargs):
+def item_save_handler(sender, **kwargs):
     if settings.DEBUG:
         print(f"{kwargs['instance']} saved.")
 
 
 @receiver(post_delete, sender=ItemArticle)
-def news_delete_handler(sender, **kwargs):
+def item_delete_handler(sender, **kwargs):
     if settings.DEBUG:
         print(f"{kwargs['instance']} deleted.")
