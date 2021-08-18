1fdebd690ec6ac8253622682e44b84d54e5c1007
raferalston <like2pwnyou@yandex.ru>
2021-08-18 14:29:47 +0300

    added debug statement

--- shop_application/manage.py
+++ shop_application/manage.py
@@ -6,7 +6,13 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.prod')
+    from config.settings._base import DEBUG
+
+    if not DEBUG:
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.prod')
+    else:
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.dev')
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
